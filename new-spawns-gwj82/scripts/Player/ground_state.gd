76181a9bcbd2ed186a2b64f0962852895bf18ad6
Sharon Nir <snir.bender@gmail.com>
2025-06-15 15:20:23 -0400

    Callout State and Mechanic Implemented

--- new-spawns-gwj82/scripts/Player/ground_state.gd
+++ new-spawns-gwj82/scripts/Player/ground_state.gd
@@ -3,8 +3,12 @@ class_name GroundState
 extends State
 
 @export var jump_velocity: float = 4.5
+
 @export var air_state: State
+@export var callout_state: State
+
 @export var jump_animation: String = "Jump"
+@export var callout_animation: String = "Callout"
 
 func state_process(delta: float) -> void:
 	if !player.is_on_floor():
@@ -13,8 +17,15 @@ func state_process(delta: float) -> void:
 func state_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and player.is_on_floor():
 		jump()
+		
+	if event.is_action_pressed("callout") and player.is_on_floor():
+		callout()
 
 func jump() -> void:
 	player.velocity.y = jump_velocity
 	next_state = air_state
 	playback.travel(jump_animation)
+
+func callout() -> void:
+	next_state = callout_state
+	playback.travel(callout_animation)
